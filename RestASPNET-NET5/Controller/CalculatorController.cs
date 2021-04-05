48a62061d3919c3d43d79d9bcbb093fd31553498
Willian Nakamura Jr <willian.nakamura.jr@gmail.com>
2021-04-05 14:02:16 -0300

    calculator

---
+++ RestASPNET-NET5/Controller/CalculatorController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestASPNET_NET5.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var firstDecimal = ConvertToDecimal(firstNumber);
+                var secondDecimal = ConvertToDecimal(secondNumber);
+
+                var sum = firstDecimal + secondDecimal;
+                var sub = firstDecimal - secondDecimal;
+                var mult = firstDecimal * secondDecimal;
+                var div = firstDecimal / secondDecimal;
+                var med = (firstDecimal * secondDecimal) / 2;
+
+                var raiz = Math.Sqrt((double)firstDecimal);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, NumberStyles.Any, 
+                NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+
+        private decimal ConvertToDecimal(string strNumber)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+    }
+}
--- RestASPNET-NET5/Startup.cs
+++ RestASPNET-NET5/Startup.cs
@@ -23,7 +23,7 @@ namespace RestASPNET_NET5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,7 +49,7 @@ namespace RestASPNET_NET5
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
