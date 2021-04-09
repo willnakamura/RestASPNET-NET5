29de2b9db5439e8d3356f0488bc1afdd445c778e
Willian Nakamura <willian.nakamura.jr@gmail.com>
2021-04-09 09:29:03 -0300

    teste

--- RestASPNET-NET5/Startup.cs
+++ RestASPNET-NET5/Startup.cs
@@ -32,6 +32,7 @@ namespace RestASPNET_NET5
         }        
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // Teste
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
