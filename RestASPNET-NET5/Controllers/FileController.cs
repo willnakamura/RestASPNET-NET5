0ec21a96c0bbf2a248326520579c4f7ac21e01ed
Willian Nakamura <willian.nakamura.jr@gmail.com>
2021-04-16 11:22:03 -0300

    Download

--- RestASPNET-NET5/Controllers/FileController.cs
+++ RestASPNET-NET5/Controllers/FileController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using RestASPNET_NET5.Business;
 using RestASPNET_NET5.Data.VO;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace RestASPNET_NET5.Controllers
@@ -19,6 +21,25 @@ namespace RestASPNET_NET5.Controllers
             _fileBusiness = fileBusiness;
         }
 
+        [HttpGet("downloadFile/{fileName}")]
+        [ProducesResponseType((200), Type = typeof(byte[]))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/octet-stream")]
+        public async Task<IActionResult> GetFileAsync(string fileName)
+        {
+            byte[] buffer = _fileBusiness.GetFile(fileName);
+            if (buffer != null)
+            {
+                HttpContext.Response.ContentType =
+                    $"application/{Path.GetExtension(fileName).Replace(".", "")}";
+                HttpContext.Response.Headers.Add("content-length", buffer.Length.ToString());
+                await HttpContext.Response.Body.WriteAsync(buffer, 0, buffer.Length);
+            }
+            return new ContentResult();
+        }
+
         [HttpPost("uploadFile")]
         [ProducesResponseType((200), Type = typeof(FileDetailVO))]
         [ProducesResponseType(400)]
@@ -28,6 +49,17 @@ namespace RestASPNET_NET5.Controllers
         {
             FileDetailVO detail = await _fileBusiness.SaveFileToDisk(file);
             return new OkObjectResult(detail);
+        }
+
+        [HttpPost("uploadMultipleFiles")]
+        [ProducesResponseType((200), Type = typeof(List<FileDetailVO>))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/json")]
+        public async Task<IActionResult> UploadManyFiles([FromForm] List<IFormFile> files)
+        {
+            List<FileDetailVO> details = await _fileBusiness.SaveFilesToDisk(files);
+            return new OkObjectResult(details);
         }
     }
 }
